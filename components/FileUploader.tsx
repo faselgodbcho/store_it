b2b2c81bf972bca48c2240411a2f3797e48d7ada
Fasel Godbcho <faselgodbcho@gmail.com>
2024-12-13 04:41:00 -0300

    Set up file uploader.

--- components/FileUploader.tsx
+++ components/FileUploader.tsx
@@ -1,3 +1,33 @@
-export default function FileUploader() {
-  return <div>FileUploader</div>;
+"use client";
+
+import React, { useCallback } from "react";
+import { useDropzone } from "react-dropzone";
+import { Button } from "./ui/button";
+import { cn } from "@/lib/utils";
+
+interface Props {
+  ownerId: string;
+  accountId: string;
+  className?: string;
+}
+
+export default function MyDropzone({ ownerId, accountId, className }: Props) {
+  const onDrop = useCallback((acceptedFiles: File[]) => {}, []);
+
+  const { getRootProps, getInputProps, isDragActive } = useDropzone({ onDrop });
+
+  return (
+    <div {...getRootProps()} className="cursor-pointer">
+      <input {...getInputProps()} />
+      <Button
+        type="button"
+        className={cn("uploader-button", className)}
+      ></Button>
+      {isDragActive ? (
+        <p>Drop the files here ...</p>
+      ) : (
+        <p>Drag 'n' drop some files here, or click to select files</p>
+      )}
+    </div>
+  );
 }
