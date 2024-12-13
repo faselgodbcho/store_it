e7618cdfbfe459516945b1ba07a9235882d38e67
Fasel Godbcho <faselgodbcho@gmail.com>
2024-12-13 15:23:29 -0300

    Implement file upload feature.

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,31 +1,41 @@
-import { Search } from "lucide-react";
+import React from "react";
+import { Button } from "@/components/ui/button";
 import Image from "next/image";
-import FileUploader from "./FileUploader";
+import Search from "@/components/Search";
+import FileUploader from "@/components/FileUploader";
 import { signOutUser } from "@/lib/actions/user.actions";
 
-export default function Header() {
+const Header = ({
+  userId,
+  accountId,
+}: {
+  userId: string;
+  accountId: string;
+}) => {
   return (
     <header className="header">
       <Search />
       <div className="header-wrapper">
-        <FileUploader />
+        <FileUploader ownerId={userId} accountId={accountId} />
+        <form
+          action={async () => {
+            "use server";
+
+            await signOutUser();
+          }}
+        >
+          <Button type="submit" className="sign-out-button">
+            <Image
+              src="/assets/icons/logout.svg"
+              alt="logo"
+              width={24}
+              height={24}
+              className="w-6"
+            />
+          </Button>
+        </form>
       </div>
-      <form
-        action={async () => {
-          "use server";
-          await signOutUser();
-        }}
-      >
-        <button type="submit" className="sign-out-button">
-          <Image
-            src="/assets/icons/logout.svg"
-            alt="logo"
-            width={24}
-            height={24}
-            className="w-6"
-          />
-        </button>
-      </form>
     </header>
   );
-}
+};
+export default Header;
